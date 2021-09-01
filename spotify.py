9a82ff6fb6605a3125019da00460b7c99820b13f
Kevin Carr <kevincarr@Kevins-MacBook-Pro.local>
2021-08-31 19:35:37 -0700

    small headers dict object refactor to be part of __init__ of spotify class

--- spotify.py
+++ spotify.py
@@ -3,10 +3,11 @@ import json
 
 class Spotify:
     def __init__(self, client_id, client_secret):
-        self.access_token = ""
-        self.base_url = "https://api.spotify.com/v1"
+        self.access_token = ''
+        self.base_url = 'https://api.spotify.com/v1'
         self.client_id = client_id
-        self.client_secret = client_secret 
+        self.client_secret = client_secret
+        self.headers = {} 
 
     def authorization(self):
         url = "https://accounts.spotify.com/api/token"
@@ -18,24 +19,19 @@ class Spotify:
         })
         response_data = auth_response.json()
 
-        self.access_token = response_data["access_token"]
+        self.access_token = response_data['access_token']
+        self.headers['Authorization'] = 'Bearer %s' % self.access_token
 
     def get_current_users_playlists(self, user_id):
         url = self.base_url + '/users/%s/playlists' % user_id
-        headers = {
-            'Authorization': 'Bearer %s' % self.access_token
-        }
 
-        r = requests.get(url, headers=headers)
+        r = requests.get(url, headers=self.headers)
         print(r.json())
 
     def get_current_users_info(self, user_id):
         url = self.base_url + '/%s' % user_id
-        headers = {
-            'Authorization': 'Bearer %s' % self.access_token
-        }
-
-        r = requests.get(url, headers=headers)
+        
+        r = requests.get(url, headers=self.headers)
         print(r.json())
 
 
